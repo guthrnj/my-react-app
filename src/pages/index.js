b003ab838d09a421e935e8992c3572911157d8c3
Nick Guthrie <NickGuthrie@Nicks-MacBook-Pro.local>
2018-07-28 16:08:24 +0800

    done some things

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
+  
   <div>
-    <h1>Currated guides</h1>
-    <p>Local Guides
-Currated maps
-Unique experiences</p>
-    <p>Sending you somewhere great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="hero">
+      <div className="heroGroup">
+        <h1>Currated guides</h1>
+        <p>Local Guides, Currated maps, Unique experiences</p>
+        <p>Sending you somewhere great.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
+
+    
   </div>
 )
 
